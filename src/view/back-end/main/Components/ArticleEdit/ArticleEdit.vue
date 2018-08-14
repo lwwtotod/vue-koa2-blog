030dc9133b84061f8caa19b716a0834acf424d76
changs <wangcs0803@gmail.com>
2018-08-14 21:34:47 +0800

    avatar style recompose

--- src/view/back-end/main/Components/ArticleEdit/ArticleEdit.vue
+++ src/view/back-end/main/Components/ArticleEdit/ArticleEdit.vue
@@ -3,10 +3,11 @@
     <div class="demo-split">
       <Split v-model="split1">
         <div slot="left" class="demo-split-pane">
-          <i-editor v-model="content"></i-editor>
+          <!-- <i-editor v-model="content"></i-editor> -->
+          <Input v-model="article.content" type="textarea" :rows="22" placeholder="Enter something..." />
         </div>
         <div slot="right" class="demo-split-pane">
-          Right Pane
+          <div style="background:#fff;margin:0 0 0 20px;height:480px;overflow-y:auto;" v-html="markedToHtml"></div>
         </div>
       </Split>
     </div>
@@ -14,11 +15,33 @@
 </template>
 
 <script>
+import jwt from "jsonwebtoken";
+import api from "@/api";
+import marked from "marked";
+import hlj from "highlight.js";
+import "highlight.js/styles/atom-one-dark.css";
 export default {
   data() {
     return {
       split1: 0.5,
-      content: ''
+      content: '',
+      article: {
+        id: null,
+        classification: null, //文章所属分类
+        founder: null, //文章标题
+        content: "" //文章内容
+      },
+    }
+  },
+  computed: {
+    markedToHtml: function () {
+      marked.setOptions({
+        highlight: function (code) {
+          return hlj.highlightAuto(code).value;
+        }
+      });
+      // console.log(this.article.content);
+      return marked(this.article.content);
     }
   },
 }
